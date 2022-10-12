5f476dd4e64ad250c5b1cd5170143da436f1295d
JinJuHa <103919453+JinJuHa@users.noreply.github.com>
2022-10-12 10:03:53 +0900

    모바일 테블릿 버튼 추가

--- src/views/main/Sidebar.vue
+++ src/views/main/Sidebar.vue
@@ -1,10 +1,10 @@
 <template>
   <div>
     <b-list-group class="sidebarList">
-      <b-list-group-item>
+      <div class="profile">
         <b-avatar button @click="$router.push('/mypage')"></b-avatar>
         <div class="nav-side-menu">{{ name }} 점장님, 반갑습니다</div>
-      </b-list-group-item>
+      </div>
       <b-list-group-item class="sidebarListB" href="#" @click="$router.push('/main')">메인 화면</b-list-group-item>
       <b-list-group-item class="sidebarListB" href="#" @click="$router.push('/main/cafelist')"
         >카페 리스트</b-list-group-item
@@ -12,6 +12,10 @@
       <b-list-group-item class="sidebarListB" href="#" @click="$router.push('/main/addcafe')"
         >지점등록</b-list-group-item
       >
+      <div>
+        <b-btn>모바일 입장</b-btn>
+        <b-btn variant="success">테블릿 입장</b-btn>
+      </div>
     </b-list-group>
   </div>
 </template>
@@ -27,6 +31,9 @@ export default {}
 }
 .sidebarListB {
   background-color: black;
+  color: #fff;
+}
+.profile {
   color: #fff;
 }
 </style>
