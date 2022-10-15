9d847783ca422ca0db895c047e94585a0fe704b8
zi12i <110373482+zi12i@users.noreply.github.com>
2022-10-15 22:51:21 +0900

    QnA plan

---
+++ src/views/user/qnaWrite.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <div>
+      <p>문의글 작성하기</p>
+    </div>
+    <div>
+      <!-- 문의글 작성파트 -->
+      <div>
+        <!-- 여기에 문의 분류 셀렉트 태그로 고를 수 있게, 제목 넣는 input -->
+      </div>
+      <div>
+        <!-- 여기에 문의사항 작성할 수 있는 textbox, validation 으로
+        제목과 내용, 문의 분류 모두 null 값이 아닌지 검사 후 작성 확인 버튼 누를 수 있게  -->
+      </div>
+      <div>
+        <!-- submit 버튼이 들어갈 자리 -->
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style scoped></style>
